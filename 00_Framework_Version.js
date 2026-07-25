d2d3dacf876f10670e5d4de42bb14b93cfb1440b
Muhammad Saeed Anser <msaeed1929@gmail.com>
2026-07-25 19:18:55 +0500

    Update framework version metadata to v3.2.0

--- 00_Framework_Version.js
+++ 00_Framework_Version.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const WEF_VERSION="1.0.0";
-
-const WEF_NAME="Workspace ERP Framework";
+const WEF_NAME = "Workspace ERP Framework";
+const WEF_API_VERSION = "3.2";
+const WEF_VERSION = "3.2.0";
+const WEF_BUILD = "20260725.001";
+const WEF_RELEASE_DATE = "2026-07-25";
+const WEF_RELEASE_CHANNEL = "Stable";
+const WEF_COPYRIGHT = "© 2026 Muhammad Saeed Anser";
